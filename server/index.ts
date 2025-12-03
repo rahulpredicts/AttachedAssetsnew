1f9a179ef4444d73945f07432707e99a0d64effb
rahulpredicts <kumarrahul46121@gmail.com>
2025-12-03 10:14:53 +0530

    Add root route

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,13 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
+
+
 const app = express();
+app.get("/", (req, res) => {
+  res.send("Backend is working 🚀");
+});
+
 
 declare module 'http' {
   interface IncomingMessage {
